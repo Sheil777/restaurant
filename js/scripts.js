fc8b62a29504b8d861b9810ea55e4e626d5216f7
Sheil <karaulov.z@mail.ru>
2021-11-14 16:10:56 +0300

    Added section our-menu

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,35 @@ $(document).ready(
         );
     }
 );
+
+new Swiper('.our-menu__slider',{
+    breakpoints: {
+        1000: {
+            slidesPerView: 3, // Количество выводимых слайдов
+            spaceBetween: 40, // Отступы между слайдами
+        },
+        850: {
+            slidesPerView: 2, // Количество выводимых слайдов
+            spaceBetween: 80, // Отступы между слайдами
+        },
+        700: {
+            slidesPerView: 2, // Количество выводимых слайдов
+            spaceBetween: 50, // Отступы между слайдами
+        }
+    },
+
+    spaceBetween: 40, // Отступы между слайдами
+
+    // Автопрокрутка
+    autoplay: {
+        // Пауза между автопрокруткой
+        delay: 5000,
+        // Закончить на последнем слайде
+        stopOnLastSlide: false,
+        // Отключить после ручного переключения
+        disableOnInteraction: true
+    },
+
+    // Скорость прокрутки
+    speed: 600,
+});
